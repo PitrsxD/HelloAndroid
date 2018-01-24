f66d4e3abf5ebcf95342df5b9906e7413d34267f
Petr Svoboda <svobodapeter@seznam.cz>
2018-01-24 02:16:50 +0100

    Initial commit

---
+++ app/src/main/java/com/svobodapeter/helloandroid/HelloAndroid.java
@@ -0,0 +1,21 @@
+package com.svobodapeter.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.View;
+import android.widget.Button;
+
+public class HelloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_android);
+    }
+
+    public void button1(View view) {
+    }
+}
